f5afbedf218ca6f810bc813eb5a307429a03121f
iuliana <iuliana@cloudsoft.io>
2024-01-08 00:14:33 +0000

    002 created the initial Maven configuration and added first sources

---
+++ chapter01/src/main/java/com/apress/bgn/one/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.apress.bgn.one;
+
+import static java.lang.System.out;
+
+public class HelloWorld {
+    public static void main(String... args) {
+        out.println("Hello World!");
+    }
+}
